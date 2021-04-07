7a047a3adc558b062ca6f26bce6fab05a47bdeba
Jhonatan-Sanchez-Pimiento <jmsanchezp@sanmateo.edu.co>
2021-04-06 22:54:26 -0500

    Inicio de Proyecto

---
+++ MyLocNet/src/mylocnet/MyLocNet.java
@@ -0,0 +1,14 @@
+
+package mylocnet;
+
+/**
+ * @JhonatanSanchez @KatianaTordecilla @NicolasSutachan
+ */
+public class MyLocNet {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
